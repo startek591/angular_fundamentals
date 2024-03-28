acde34beb64926488843bed26527dd1466b21ff7
jonathan_fambro <startek591@gmail.com>
2024-03-28 13:55:14 -0400

    Defining a Component

--- Essentials/src/app/app.component.ts
+++ Essentials/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TodoListItem } from './todo-list-item/todo-list-item.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, TodoListItem],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Essentials';
