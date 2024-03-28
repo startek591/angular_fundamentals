db5cffe62fdb8863f90d58f3bea72bbccfec8887
jonathan_fambro <startek591@gmail.com>
2024-03-28 15:32:03 -0400

    Rendering Dynamic Data

--- Essentials/src/app/todo-list-item/todo-list-item.component.ts
+++ Essentials/src/app/todo-list-item/todo-list-item.component.ts
@@ -3,18 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   standalone: true,
   selector: 'todo-list-item',
-  templateUrl: './todo-list-item.component.html',
+  template: ` <p>Title: {{ taskTitle }}</p> `,
   styleUrl: './todo-list-item.component.css',
 })
 export class TodoListItem {
-  taskTitle = '';
-  isComplete = false;
-
-  completeTask() {
-    this.isComplete = true;
-  }
-
-  updateTitle(newTitle: string) {
-    this.taskTitle = newTitle;
-  }
+  taskTitle = 'Read cup of coffee';
 }
