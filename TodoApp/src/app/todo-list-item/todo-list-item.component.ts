5e841eb0b2f024ca935b4ed4cf5e68656c3014b1
jonathan_fambro <startek591@gmail.com>
2024-10-27 12:31:54 -0400

    Defining a Component

---
+++ TodoApp/src/app/todo-list-item/todo-list-item.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'todo-list-item',
+  standalone: true,
+  template: ` <li>(TODO) Read Angular Essentials Guide</li> `,
+  styles: `
+    li {
+        color: red;
+        font-weight: 300
+    }
+  `,
+})
+export class TodoListItem {}
