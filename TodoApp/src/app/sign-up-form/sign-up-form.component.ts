3b9533b3563d2b94edf36ea0e65a6becfb9f2b61
jonathan_fambro <startek591@gmail.com>
2024-10-27 12:47:54 -0400

    Dynamic Attributes

--- TodoApp/src/app/sign-up-form/sign-up-form.component.ts
+++ TodoApp/src/app/sign-up-form/sign-up-form.component.ts
@@ -2,14 +2,19 @@
 // property is wrapped in square brackets to inform Angular that the declared value should
 // be interpreted as a JavaScript statement.
 
+// Dynamically bind custom HTML attributes, you might be inclined to wrap the custom attributes
+// with the same square brackets.
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'sign-up-form',
   template: `
+    <button [attr.data-test-id]="testId">Primary CTA</button>
     <button type="submit" [disabled]="formIsInvalid">Submit</button>
   `,
 })
 export class SignUpForm {
   formIsInvalid = true;
+  testId = 'main-cta';
 }
