96450cfef7a65c5e8df0b9053c6abfb319c620b4
jonathan_fambro <startek591@gmail.com>
2025-01-17 14:49:58 -0500

    Created Product display Component and implement it in Product Row Component

--- how-angular-works/inventory-app/src/app/product-row/product-row.component.ts
+++ how-angular-works/inventory-app/src/app/product-row/product-row.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input, HostBinding } from '@angular/core';
 import { Product } from '../models/product.model';
 import { ProductImageComponent } from '../product-image/product-image.component';
+import { PriceDisplayComponent } from '../product-display/product-display.component';
 
 @Component({
   selector: 'app-product-row',
   standalone: true,
   templateUrl: './product-row.component.html',
-  imports: [ProductImageComponent],
+  imports: [ProductImageComponent, PriceDisplayComponent],
 })
 export class ProductRowComponent {
   @Input() product!: Product;
