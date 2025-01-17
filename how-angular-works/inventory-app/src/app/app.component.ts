8e8c042f977ada4ef8cfc834e1b5cb4f032c8643
jonathan_fambro <startek591@gmail.com>
2025-01-17 14:25:52 -0500

    Fixed product image path

--- how-angular-works/inventory-app/src/app/app.component.ts
+++ how-angular-works/inventory-app/src/app/app.component.ts
@@ -17,21 +17,21 @@ export class AppComponent {
       new Product(
         'MYSHOES',
         'Black Running Shoes',
-        'public/images/products/black-shoes.jpg',
+        'images/products/black-shoes.jpg',
         ['Men', 'Shoes', 'Running Shoes'],
         109.99
       ),
       new Product(
         'NEATOJACKET',
         'Blue Jacket',
-        'public/images/product/blue-jacket.jpg',
+        'images/products/blue-jacket.jpg',
         ['Women', 'Apparel', 'Jackets & Vests'],
         238.99
       ),
       new Product(
         'NICEHAT',
         'A Nice Black Hat',
-        'public/images/products/black-hat.jpg',
+        'images/products/black-hat.jpg',
         ['Men', 'Accessories', 'Hats'],
         29.99
       ),
