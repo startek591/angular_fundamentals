c08ffba6406f0430ee0b10b4df44a4bbb788aa19
jonathan_fambro <startek591@gmail.com>
2025-01-17 16:46:20 -0500

    Created Intro Component

---
+++ built-in-directives/src/app/intro/intro.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-intro',
+  standalone: true,
+  templateUrl: './intro.component.html',
+})
+export class IntroComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
