0b1d548f675ea2e1980d226f710b693b220f671d
jonathan_fambro <startek591@gmail.com>
2025-01-17 14:27:46 -0500

    Created a product-image and implemented it in the product row component

--- how-angular-works/inventory-app/src/app/product-row/product-row.component.ts
+++ how-angular-works/inventory-app/src/app/product-row/product-row.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input, HostBinding } from '@angular/core';
 import { Product } from '../models/product.model';
+import { ProductImageComponent } from '../product-image/product-image.component';
 
 @Component({
   selector: 'app-product-row',
   standalone: true,
   templateUrl: './product-row.component.html',
+  imports: [ProductImageComponent],
 })
 export class ProductRowComponent {
   @Input() product!: Product;
