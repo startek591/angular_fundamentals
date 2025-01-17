a62bbac2bc1afe33f81b5e880641462e62016156
jonathan_fambro <startek591@gmail.com>
2025-01-17 14:59:44 -0500

    Implement it into the product row component

--- how-angular-works/inventory-app/src/app/product-row/product-row.component.ts
+++ how-angular-works/inventory-app/src/app/product-row/product-row.component.ts
@@ -2,12 +2,17 @@ import { Component, Input, HostBinding } from '@angular/core';
 import { Product } from '../models/product.model';
 import { ProductImageComponent } from '../product-image/product-image.component';
 import { PriceDisplayComponent } from '../product-display/product-display.component';
+import { ProductDepartmentComponent } from '../product-department/product-department.component';
 
 @Component({
   selector: 'app-product-row',
   standalone: true,
   templateUrl: './product-row.component.html',
-  imports: [ProductImageComponent, PriceDisplayComponent],
+  imports: [
+    ProductImageComponent,
+    PriceDisplayComponent,
+    ProductDepartmentComponent,
+  ],
 })
 export class ProductRowComponent {
   @Input() product!: Product;
