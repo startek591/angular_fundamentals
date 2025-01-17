a4b3b32641da73d6d592d5a64e160714646f0fb3
jonathan_fambro <startek591@gmail.com>
2025-01-17 16:56:56 -0500

    Implemented the route for the intro component

--- built-in-directives/src/app/app.routes.ts
+++ built-in-directives/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ExampleDef } from '../model/example.model';
+import { IntroComponent } from './intro/intro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    label: 'Intro',
+    name: 'Root',
+    path: '',
+    component: IntroComponent,
+  } as ExampleDef,
+];
