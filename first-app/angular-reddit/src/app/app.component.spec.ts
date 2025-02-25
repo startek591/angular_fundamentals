069cc16fd7f108224af546e2f73c75c6cda367cc
jonathan_fambro <startek591@gmail.com>
2025-02-25 18:48:54 -0500

    Removed template

--- first-app/angular-reddit/src/app/app.component.spec.ts
+++ first-app/angular-reddit/src/app/app.component.spec.ts
@@ -13,17 +13,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have the 'angular-reddit' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-reddit');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-reddit');
-  });
 });
