18c3bbb6fe8d70599711f42249470cf795b4059d
jonathan_fambro <startek591@gmail.com>
2025-02-25 19:42:02 -0500

    Created a article model and setup the app component

--- first-app/angular-reddit/src/app/app.component.spec.ts
+++ first-app/angular-reddit/src/app/app.component.spec.ts
@@ -1,16 +1,45 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
-    }).compileComponents();
+    });
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    const component = fixture.componentInstance;
+    expect(component).toBeTruthy();
+  });
+
+  it('should initialize the articles array with 3 items', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const component = fixture.componentInstance;
+    expect(component.article.length).toEqual(3);
+  });
+
+  it('should add a new article when user clicks on submit link button', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const component = fixture.componentInstance;
+    const titleInput = { value: 'Angular 2' } as HTMLInputElement;
+    const linkInput = { value: 'http://angular.io' } as HTMLInputElement;
+    component.addArticle(titleInput, linkInput);
+    expect(component.article.length).toEqual(4);
+  });
+
+  it('should return a sorted array of articles', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const component = fixture.componentInstance;
+    const titleInput = { value: 'Angular 2' } as HTMLInputElement;
+    const linkInput = { value: 'http://angular.io' } as HTMLInputElement;
+    component.addArticle(titleInput, linkInput);
+    const sortedArticles = component.sortedArticles();
+    expect(sortedArticles[0].votes).toBeGreaterThan(sortedArticles[1].votes);
   });
 });
--- first-app/angular-reddit/src/app/app.component.ts
+++ first-app/angular-reddit/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,26 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  article: Article[];
+
+  constructor() {
+    this.article = [
+      new Article('Angular 2', 'http://angular.io', 3),
+      new Article('Fullstack', 'http://fullstack.io', 2),
+      new Article('Angular Homepage', 'http://angular.io', 1),
+    ];
+  }
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.article.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
+    return false;
+  }
+
+  sortedArticles(): Article[] {
+    return this.article.sort((a: Article, b: Article) => b.votes - a.votes);
+  }
+}
