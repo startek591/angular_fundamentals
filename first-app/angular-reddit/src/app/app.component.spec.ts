27abb51aa6f8aefc5093a2926f5dbb668fad0394
jonathan_fambro <startek591@gmail.com>
2025-03-17 13:47:58 -0400

    Created App Component and Testing Suite and Implemented Article Component

--- first-app/angular-reddit/src/app/app.component.spec.ts
+++ first-app/angular-reddit/src/app/app.component.spec.ts
@@ -1,29 +1,56 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { Article } from './article/article.model';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
   });
 
-  it(`should have the 'angular-reddit' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-reddit');
+  it('should have articles', () => {
+    expect(component.articles.length).toBe(3);
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-reddit');
+  it('should add an new article', () => {
+    const titleInput = document.createElement('input') as HTMLInputElement;
+    const linkInput = document.createElement('input') as HTMLInputElement;
+
+    titleInput.value = 'Test';
+    linkInput.value = 'https://test.com';
+
+    const result = component.addArticle(titleInput, linkInput);
+
+    expect(component.articles.length).toBe(4);
+    expect(component.articles[3].title).toEqual('Test');
+    expect(component.articles[3].link).toEqual('https://test.com');
+
+    expect(titleInput.value).toBe('');
+    expect(linkInput.value).toBe('');
+
+    expect(result).toBe(false);
+  });
+
+  it('should return articles sorted by votes in descending order', () => {
+    const article1 = new Article('Article 1', 'https://example.com/1', 5);
+    const article2 = new Article('Article 2', 'https://example.com/2', 10);
+    const article3 = new Article('Article 3', 'https://example.com/3', 7);
+    component.articles = [article1, article2, article3];
+
+    const sortedArticles = component.sortedArticles();
+
+    expect(sortedArticles[0]).toBe(article2);
+    expect(sortedArticles[1]).toBe(article3);
+    expect(sortedArticles[2]).toBe(article1);
   });
 });
--- first-app/angular-reddit/src/app/app.component.ts
+++ first-app/angular-reddit/src/app/app.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Article } from './article/article.model';
+import { ArticleComponent } from './article/article.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NgFor, ArticleComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-reddit';
+  articles: Article[];
+
+  constructor() {
+    this.articles = [
+      new Article('Angular', 'http://angular.io', 3),
+      new Article('Fullstack', 'http://fullstack.io', 2),
+      new Article('Angular Homepage', 'http://angular.io', 1),
+    ];
+  }
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
+    return false;
+  }
+
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+  }
 }
